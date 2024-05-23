769fd40b99bb052d0856227601b70ec7cec4d8dd
frankrazzih <francischege602@gmail.com>
2024-05-24 00:58:05 +0300

    advanced mysql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+--create a unique user
+CREATE TABLE IF NOT EXISTS users ( 
+    id INT NOT NUL AUTO_INCREMENT,
+    email VARCHAR (255) NOT NULL UNIQUE,
+    name VARCHAR (255),
+    PRIMARY KEY (id)
+);
