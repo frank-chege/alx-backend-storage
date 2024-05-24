f4542ae5ac1b4ccad2ee670a5f4913edbb40010c
frankrazzih <francischege602@gmail.com>
2024-05-24 21:00:53 +0300

    advanced mysql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,6 @@
 --checks for the lifes[an of bands with a style named glam rock.
 SELECT band_name,
-    CASE 
-    WHEN split > 2022 OR split=0 OR split IS NULL THEN 2022-formed
-    ELSE split-formed
-    END AS lifespan
+    (IFNULL(split, '2022') - formed) as lifespan
     FROM metal_bands
-    WHERE style LIKE '%Glam rock%' 
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0 
     ORDER BY lifespan DESC;
