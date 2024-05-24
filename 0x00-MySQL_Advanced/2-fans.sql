cc2d02e9a8b1d6df62cf7c048c8c8fc42cec7313
frankrazzih <francischege602@gmail.com>
2024-05-24 15:36:14 +0300

    advanced mysql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 --rank country origins ordered by no. of fans
-SELECT origin , fans AS nb_fans FROM metal_bands ORDER BY fans DESC
+SELECT origin , SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
