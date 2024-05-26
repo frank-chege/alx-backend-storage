aa6844ecdefea11bcdc64856692daa8eacfb9d75
frankrazzih <francischege602@gmail.com>
2024-05-27 00:56:53 +0300

    advanced mysql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,5 +4,4 @@ CREATE VIEW need_mmeting AS
 SELECT name 
     FROM students
 WHERE score < 80 
-    AND last_meeting IS NULL
-    OR last_meeting < CURDATE() - INTERVAL 1 MONTH;
+    AND (last_meeting IS NULL OR last_meeting < CURDATE() - INTERVAL 1 MONTH);
