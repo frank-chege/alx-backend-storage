5cec94ae890871efe94a5a6613081c358fbfe9ee
frankrazzih <francischege602@gmail.com>
2024-05-26 23:52:10 +0300

    advanced mysql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,10 +1,12 @@
 -- safe divide function to avoid 0 divide errors
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-CREATE FUNCTION SafeDiv(a INT, b INT)
-IF b == 0 THEN
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS INT
+BEGIN
+IF b = 0 THEN
     RETURN 0;
 ELSE
     RETURN a/b;
 END IF;
+END $$
 DELIMITER ;
