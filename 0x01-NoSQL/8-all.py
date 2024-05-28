d9c43aef1516cd1ef4c64f182b89dd0a6baac2f7
frankrazzih <francischege602@gmail.com>
2024-05-28 12:25:25 +0300

    MongoDB

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''list docs in collection'''
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    '''list docs in collection'''
+    client = MongoClient('localhost', 27017)
+    db = client['my_db']
+    result = db.mongo_collection.find()
