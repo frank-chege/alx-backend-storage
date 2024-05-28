0cdb3933c086297bcc33b08fb0384d978e937de9
frankrazzih <francischege602@gmail.com>
2024-05-28 16:58:21 +0300

    MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,21 +6,21 @@ client = MongoClient('localhost', 27017)
 db  = client['logs']
 nginx = db['nginx'] #collection
 
-def count_no(kwargs):
+def count_no(**kwargs):
     '''counts the records in the db'''
     count = db.nginx.count_documents(kwargs)
     return count
 
 def main():
     '''show some nginx stats'''
-    print(count_no({}), 'logs')
+    print(count_no(), 'logs')
     print(f"Methods:\n\
-    method GET: {count_no({'method': 'GET'})}\n\
-    method POST: {count_no({'method': 'POST'})}\n\
-    method PUT: {count_no({'method': 'PUT'})}\n\
-    method PATCH: {count_no({'method': 'PATCH'})}\n\
-    method DELETE: {count_no({'method': 'DELETE'})}")
-    print(count_no({'path': '/status'}), 'status check')
+    method GET: {count_no(method='GET')}\n\
+    method POST: {count_no(method='POST')}\n\
+    method PUT: {count_no(method='PUT')}\n\
+    method PATCH: {count_no(method='PATCH')}\n\
+    method DELETE: {count_no(method='DELETE')}")
+    print(count_no(path='/status'), 'status check')
 
 if __name__ == '__main__':
     main()
