e1a8c35394f66c5cdd0142fa7aad7b70f9860627
frankrazzih <francischege602@gmail.com>
2024-05-28 15:33:36 +0300

    MongoDB

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''find schools with a specific topic'''
+
+from pymongo import MongoClient
+
+def schools_by_topic(mongo_collection, topic):
+    '''find schools with a specific topic'''
+    result = mongo_collection.find({'topics': topic})
