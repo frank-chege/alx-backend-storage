c4268e6d50d0acb94d8d2f8bd13f2b0d05edb0a7
frankrazzih <francischege602@gmail.com>
2024-05-28 13:23:29 +0300

    MongoDB

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,11 +4,5 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection):
     '''list docs in collection'''
-    client = MongoClient('localhost', 27017)
-    db = client['my_db']
-    result = db.mongo_collection.find()
-    if result is None:
-        return result
-    else:
-        for doc in result:
-            yield doc
+    result = mongo_collection.find()
+    return list(result)
